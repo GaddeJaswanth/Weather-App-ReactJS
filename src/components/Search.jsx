5f25c8096eefc1950d165b13660a26a404695ba9
GaddeJaswanth <jaswanthgadde626@gmail.com>
2023-07-10 05:44:40 +0530

    Weather details added successfully

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -33,9 +33,9 @@ export default function Search(){
     }
 
     return (
-        <div align="center" id="mainDiv">
+        <div align="center">
             <div className="md:flex lg:flex justify-center mt-5 sm:block">
-                <input type="text" className="mr-5 px-5 py-5 h-5" value={city} placeholder="Enter Location" onChange={(e)=>{
+                <input type="text" className="mr-5 px-7 py-5 h-5" value={city} placeholder="Enter Location" onChange={(e)=>{
                     setCity(e.target.value);
                 }}/>
                 <BsSearch className="cursor-pointer mt-2" size={25} onClick={()=>{
@@ -49,24 +49,50 @@ export default function Search(){
                 </div>
             ):(
                 //Current Weather
-                <div>
-                    <h1>Current Weather</h1>
+                <div className="mt-6">
+                    <h1 className="text-3xl font-bold p-5">Current Weather</h1>
                     <div className="md:flex lg:flex text-center mt-5 w-[90%] sm:block rounded-lg">
-                    <div className="w-[45%] mr-[10%] rounded-lg text-black bg-white">
-                        <h1>{data.name.toUpperCase()}</h1>
-                        <h1>{data.main.temp} °C</h1>
-                        <img align="center" src={"https://openweathermap.org/img/wn/"+ data.weather[0].icon + ".png"} alt="icon" />
-                        <h2>Feels Like : {data.main.feels_like} °C</h2>
-                        <h4>{data.weather[0].main}</h4>
-                        <p>{data.weather[0].description}</p>
+                        <div className="ml-[4%] p-5 lg:w-[40%] mr-[10%] rounded-lg text-white bg-gray-600 shadow-lg">
+                            <table align="center">
+                                <tr>
+                                    <td><h1 className="text-xl">{data.name.toUpperCase()}</h1></td>
+                                </tr>
+                                <tr>
+                                    <td className="p-5"><h1 className="text-5xl">{data.main.temp} °C</h1></td>
+                                </tr>
+                                <tr>
+                                    <td align="center"><img align="center" src={"https://openweathermap.org/img/wn/"+ data.weather[0].icon + "@4x.png"} alt="icon" /></td>
+                                </tr>
+                                <tr><td><h3 className="text-3xl">{data.weather[0].main}</h3></td></tr>
+                            </table>
+                        </div>
+                            <table align="center" className="bg-gray-600 text-white lg:w-[40%] rounded-lg text-center shadow-lg">
+                                <tr>
+                                    <td><h1 className="text-xl">Description</h1></td>
+                                    <td><h1 className="text-xl">{data.weather[0].description.toUpperCase()}</h1></td>
+                                </tr>
+                                <tr>
+                                    <td><h1 className="text-xl">Feels Like</h1></td>
+                                    <td><h1 className="text-2xl">{data.main.feels_like} °C</h1></td>
+                                </tr>
+                                <tr>
+                                    <td><h1 className="text-xl">Min Temp</h1></td>
+                                    <td><h1 className="text-2xl">{data.main.temp_min} °C</h1></td>
+                                </tr>
+                                <tr>
+                                    <td><h1 className="text-xl">Max Temp</h1></td>
+                                    <td><h1 className="text-2xl">{data.main.temp_max} °C</h1></td>
+                                </tr>
+                                <tr>
+                                    <td><h1 className="text-xl">Pressure</h1></td>
+                                    <td><h1 className="text-2xl">{data.main.pressure}</h1></td>
+                                </tr>
+                                <tr>
+                                    <td><h1 className="text-xl">Humidity</h1></td>
+                                    <td><h1 className="text-2xl">{data.main.humidity}</h1></td>
+                                </tr>
+                            </table>
                     </div>
-                    <div className="bg-white text-black w-[45%] rounded-lg">
-                        <h1>Min Temp : {data.main.temp_min}</h1>
-                        <h1>Max Temp : {data.main.temp_max}</h1>
-                        <h1>Pressure : {data.main.pressure}</h1>
-                        <h1>Humidity : {data.main.humidity}</h1>
-                    </div>
-                </div>
                 </div>
             )
             }
