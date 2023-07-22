85cc19552c2873f7edb14f4357e94ed434f3ecec
GaddeJaswanth <jaswanthgadde626@gmail.com>
2023-07-22 19:12:55 +0530

    Background Changed

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import {TiWeatherStormy} from 'react-icons/ti'
+export default function Header(){
+    return ( 
+        <div>
+            <TiWeatherStormy size={50} color='yellow'/>
+        </div>
+    )
+}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import {BsSearch} from 'react-icons/bs';
+import Header from "./Header";
+import {MdNotes} from 'react-icons/md';
+import {TbTemperatureCelsius} from 'react-icons/tb';
+import {HiMiniArrowDown, HiMiniArrowUp} from 'react-icons/hi2';
+import {AiOutlineCompress} from 'react-icons/ai';
+import {WiHumidity} from 'react-icons/wi';
 
 export default function Search(){
 
     var [city, setCity] = useState("");
-    var [data, setData] = useState({"coord":{"lon":80.75,"lat":16.2713},
-    "weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations",
-    "main":{"temp":37.17,"feels_like":42.86,"temp_min":37.06,"temp_max":37.17,"pressure":1003,"humidity":43,"sea_level":1003,"grnd_level":1002},
-    "visibility":10000,"wind":{"speed":4.62,"deg":287,"gust":6.34},
-    "clouds":{"all":91},"dt":1688891661,"sys":{"type":2,"id":2083890,"country":"IN","sunrise":1688861429,"sunset":1688908402},
-    "timezone":19800,"id":1253077,"name":"Kollipara","cod":200});
+    var [data, setData] = useState(null);
 
     const getData = () => {
         const url = `https://api.openweathermap.org/data/2.5/weather?appid=c69b7b0d4facca47fbc79d53857e5c7b&units=metric&q=` + city;
@@ -34,67 +35,73 @@ export default function Search(){
 
     return (
         <div align="center">
-            <div className="md:flex lg:flex justify-center mt-5 sm:block">
-                <input type="text" className="mr-5 px-7 py-5 h-5" value={city} placeholder="Enter Location" onChange={(e)=>{
+            <div className="flex ml-7 mt-2">
+                <Header/>
+                <h1 className="text-3xl font-semibold m-2 ml-6 text-white">Weather using ReactJS</h1>
+                
+            </div>
+            <div className="flex justify-center mt-5">
+                <input type="text" className="mr-6 px-10 py-5 h-5 rounded-3xl" value={city} placeholder="Enter Location" onChange={(e)=>{
                     setCity(e.target.value);
                 }}/>
-                <BsSearch className="cursor-pointer mt-2" size={25} onClick={()=>{
+                <BsSearch className="cursor-pointer mt-1" size={30} onClick={()=>{
                     getData();
                 }}/>
+                
             </div>
             {
                 !Boolean(data) ? (
                 <div className="text-white bg-black mt-10 w-[95%] text-2xl p-10">
                     <h1>Enter a valid Location</h1>
                 </div>
-            ):(
+                ):(
                 //Current Weather
-                <div className="mt-6">
-                    <h1 className="text-3xl font-bold p-5">Current Weather</h1>
-                    <div className="md:flex lg:flex text-center mt-5 w-[90%] sm:block rounded-lg">
-                        <div className="ml-[4%] p-5 lg:w-[40%] mr-[10%] rounded-lg text-white bg-gray-600 shadow-lg">
-                            <table align="center">
+                    <div className="mt-6">
+                        <h1 className="text-3xl font-bold p-4 text-white">Current Weather</h1>
+                        <div className="md:flex text-center mt-5 w-[90%] sm:block">
+                            <div id="weather" className="ml-[4%] p-6 md:w-[40%] mr-[10%] rounded-lg text-white shadow-lg sm:w-[90%]">
+                                <table align="center">
+                                    <tr>
+                                        <td><h1 className="text-2xl">{data.name.toUpperCase()}</h1></td>
+                                    </tr>
+                                    <tr>
+                                        <td className="p-5"><h1 className="text-5xl">{data.main.temp} °C</h1></td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center"><img src={"https://openweathermap.org/img/wn/"+ data.weather[0].icon + "@4x.png"} alt="icon" /></td>
+                                    </tr>
+                                    <tr><td><h3 className="text-3xl">{data.weather[0].main}</h3></td></tr>
+                                </table>
+                            </div>
+                            <table id="details" className="ml-[4%] text-white md:w-[40%] rounded-lg shadow-lg">
                                 <tr>
-                                    <td><h1 className="text-xl">{data.name.toUpperCase()}</h1></td>
-                                </tr>
-                                <tr>
-                                    <td className="p-5"><h1 className="text-5xl">{data.main.temp} °C</h1></td>
-                                </tr>
-                                <tr>
-                                    <td align="center"><img align="center" src={"https://openweathermap.org/img/wn/"+ data.weather[0].icon + "@4x.png"} alt="icon" /></td>
-                                </tr>
-                                <tr><td><h3 className="text-3xl">{data.weather[0].main}</h3></td></tr>
-                            </table>
-                        </div>
-                            <table align="center" className="bg-gray-600 text-white lg:w-[40%] rounded-lg text-center shadow-lg">
-                                <tr>
-                                    <td><h1 className="text-xl">Description</h1></td>
+                                    <td>{<MdNotes size={30} className="inline mr-1" color="lightgreen"/>} <h1 className="text-xl inline"> Description</h1></td>
                                     <td><h1 className="text-xl">{data.weather[0].description.toUpperCase()}</h1></td>
                                 </tr>
                                 <tr>
-                                    <td><h1 className="text-xl">Feels Like</h1></td>
+                                    <td>{<TbTemperatureCelsius size={30} className="inline mr-1" color="lightgreen"/>} <h1 className="text-xl inline">Feels Like</h1></td>
                                     <td><h1 className="text-2xl">{data.main.feels_like} °C</h1></td>
                                 </tr>
                                 <tr>
-                                    <td><h1 className="text-xl">Min Temp</h1></td>
+                                    <td>{<HiMiniArrowDown size={30} className="inline mr-1" color="lightgreen"/>} <h1 className="text-xl inline">Min Temp</h1></td>
                                     <td><h1 className="text-2xl">{data.main.temp_min} °C</h1></td>
                                 </tr>
                                 <tr>
-                                    <td><h1 className="text-xl">Max Temp</h1></td>
+                                    <td>{<HiMiniArrowUp size={30} className="inline mr-1" color="lightgreen"/>} <h1 className="text-xl inline">Max Temp</h1></td>
                                     <td><h1 className="text-2xl">{data.main.temp_max} °C</h1></td>
                                 </tr>
                                 <tr>
-                                    <td><h1 className="text-xl">Pressure</h1></td>
+                                    <td>{<AiOutlineCompress size={30} className="inline mr-1" color="lightgreen"/>} <h1 className="text-xl inline">Pressure</h1></td>
                                     <td><h1 className="text-2xl">{data.main.pressure}</h1></td>
                                 </tr>
                                 <tr>
-                                    <td><h1 className="text-xl">Humidity</h1></td>
+                                    <td>{<WiHumidity size={30} className="inline mr-1" color="lightgreen"/>} <h1 className="text-xl inline">Humidity</h1></td>
                                     <td><h1 className="text-2xl">{data.main.humidity}</h1></td>
                                 </tr>
                             </table>
+                        </div>
                     </div>
-                </div>
-            )
+                )
             }
         </div>
     )
